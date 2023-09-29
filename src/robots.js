46d09eb17fea12716e036a67b8f471dfe988677c
easigrafiti19 <machaevilli96@gmail.com>
2023-09-29 08:58:32 +0100

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Lanne Fraghan',
+        username: 'Bresti',
+        email: 'Lanneresti@outlook.com'
+    },
+    {
+        id: 2,
+        name: 'Sam Larry',
+        username: 'Marlian',
+        email: 'Naira@outlook.com'
+    },
+    {
+        id: 3,
+        name: 'Shalipoppi Bennin',
+        username: 'Obapluto',
+        email: 'Prestigiously@outlook.com'
+    },
+    {
+        id: 4,
+        name: 'Lanselot Merci',
+        username: 'Bucanio',
+        email: 'Lanrecy@outlook.com'
+    },
+    {
+        id: 5,
+        name: 'Drake Whorehouse',
+        username: 'Dodomayana',
+        email: 'Pussysmells@outlook.com'
+    },
+    {
+        id: 6,
+        name: 'Mateo Karakas',
+        username: 'Brozovic',
+        email: 'Bugalili@outlook.com'
+    },
+    {
+        id: 7,
+        name: 'Desty Fraghan',
+        username: 'Desty',
+        email: 'Desty@outlook.com'
+    },
+    {
+        id: 8,
+        name: 'Tony Sporanos',
+        username: 'Lovota',
+        email: 'Dierybreed@outlook.com'
+    },
+    {
+        id: 9,
+        name: 'Adam Smith',
+        username: 'clove',
+        email: 'Dustpan@outlook.com'
+    },
+    {
+        id: 10,
+        name: 'Kristin Kreuk',
+        username: 'Kraccy',
+        email: 'Lancycrackwhore@outlook.com'
+    },
+]
